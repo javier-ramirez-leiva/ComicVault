ff9a2932d0c279c95354a98a2befd85a118412b9
javier-ramirez-leiva <javier.ramirez.leiva0@gmail.com>
2026-02-20 19:31:28 +0100

    Delete all exceptions front

--- frontend/src/app/components/exceptions-page/exceptions-page.component.ts
+++ frontend/src/app/components/exceptions-page/exceptions-page.component.ts
@@ -1,12 +1,15 @@
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Observable, switchMap } from 'rxjs';
+import { combineLatest, Observable, startWith, Subject, switchMap } from 'rxjs';
 import { Exception } from 'src/app/interfaces/exception';
 import { ExceptionService } from 'src/app/services/exception.service';
 import { PageNavigatorComponent } from '../page-navigator/page-navigator.component';
 import { ExceptionsTableComponent } from '../exceptions-table/exceptions-table.component';
 import { CommonModule } from '@angular/common';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { S } from 'node_modules/@angular/cdk/scrolling-module.d-ud2XrbF8';
 
+@UntilDestroy()
 @Component({
   selector: 'app-exceptions-page',
   imports: [PageNavigatorComponent, ExceptionsTableComponent, CommonModule],
@@ -18,11 +21,15 @@ export class ExceptionsPageComponent {
   private readonly router: Router = inject(Router);
 
   exceptions$: Observable<Exception[]>;
+  private readonly triggerReload$ = new Subject<void>();
   page: number = 1;
 
   constructor() {
-    this.exceptions$ = this.route.queryParams.pipe(
-      switchMap((params) => {
+    this.exceptions$ = combineLatest([
+      this.route.queryParams,
+      this.triggerReload$.pipe(startWith(null)),
+    ]).pipe(
+      switchMap(([params, _]) => {
         this.page = params['page'] ? params['page'] : 1;
         return this.exceptionService.getExceptions(this.page);
       }),
@@ -32,6 +39,15 @@ export class ExceptionsPageComponent {
   onPageChange(page: number) {
     this.page = page;
     this.navigate();
+  }
+
+  deleteAllExceptions() {
+    this.exceptionService
+      .deleteAll()
+      .pipe(untilDestroyed(this))
+      .subscribe(() => {
+        this.triggerReload$.next();
+      });
   }
 
   private navigate() {
