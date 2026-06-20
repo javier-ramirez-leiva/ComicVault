b9fba66b8602d8c7f30721df0288be0759514f2a
javier-ramirez-leiva <javier.ramirez.leiva0@gmail.com>
2026-06-20 12:35:56 +0200

    Use date on back filterint

--- backend/src/main/java/org/comicVaultBackend/domain/regular/FilterHistory.java
+++ backend/src/main/java/org/comicVaultBackend/domain/regular/FilterHistory.java
@@ -6,14 +6,16 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.comicVaultBackend.domain.entities.HistoryEntity;
 
+import java.util.Date;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class FilterHistory {
     private String comicTitle;
-    private String dateStart;
-    private String dateEnd;
+    private Date dateStart;
+    private Date dateEnd;
     private boolean readStatusOnGoing;
     private boolean readStatusRead;
     private boolean inLibraryYes;
@@ -33,6 +35,13 @@ public class FilterHistory {
             return false;
         }
         if (!historyEntity.getAlive() && !inLibraryNo) {
+            return false;
+        }
+        if (dateStart != null && historyEntity.getProgress_date().before(dateStart)) {
+            return false;
+        }
+
+        if (dateEnd != null && historyEntity.getProgress_date().after(dateEnd)) {
             return false;
         }
         return true;
